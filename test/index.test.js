1be4d8bd74f18afcfa939103f4f7f7a288dfd1ff
Alef Oliveira <alef.oliveira@gertec.com.br>
2022-08-07 20:31:51 -0300

    Add new test case

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,21 @@
 const getFile = require('../index');
 
-test('Must be a function', () => {
-    expect(typeof getFile).toBe('function');
+const ASSETS_PATH = './test/files/';
+const ASSET_FILEPATH_1 = ASSETS_PATH + 'test-text1.md';
+const ASSET_FILEPATH_2 = ASSETS_PATH + 'test-text2.md';
+const ARRAY_RESULT_1 = [
+    {
+        FileList: 'https://developer.mozilla.org/pt-BR/docs/Web/API/FileList'
+    }
+]
+
+describe('getFile::', () => {
+    it('Must be a function', () => {
+        expect(typeof getFile).toBe('function');
+    });
+    it('Must be return results array::', async () => {
+        const result = await getFile(ASSET_FILEPATH_1); 
+        expect(result).toEqual(ARRAY_RESULT_1);
+    });
 });
+
