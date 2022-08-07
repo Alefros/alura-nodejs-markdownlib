b3f3a3834f61e852ef9d22536e84cb244cac3eb3
Alef Oliveira <alef.oliveira@gertec.com.br>
2022-08-07 18:15:33 -0300

    Performs identation

--- http-validation.js
+++ http-validation.js
@@ -1,15 +1,20 @@
 const fetch = require('node-fetch');
 
 async function statusCheck(urls) {
-    const arrayStatus = await Promise.all(urls.map(async url => {
-            const res = await fetch(url);
-            return res.status;
+    const arrayStatus = await Promise
+        .all(urls
+            .map(async url => {
+                const res = await fetch(url);
+                return res.status;
     }));
     return arrayStatus;
 }
 
 function extractUrls(links) {
-    return links.map(objectLink =>  Object.values(objectLink).join());
+    return links
+        .map(objectLink =>  Object
+            .values(objectLink)
+            .join());
 }
 
 async function urlsValidator(links) {
