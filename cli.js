1b01b2f6a2dc77bd6f6c830a87f005086cc98414
Alef Oliveira <alef.oliveira@gertec.com.br>
2022-08-07 02:16:47 -0300

    Format output of links

--- cli.js
+++ cli.js
@@ -5,7 +5,7 @@ const path = process.argv;
 
 async function processText(filePath) {
     const result = await getFileAsync(path[2]);
-    console.log(chalk.yellow('Links list', JSON.stringify(result)));
+    console.log(chalk.yellow('Links list: \n', JSON.stringify(result)));
 }
 
 processText(path);
