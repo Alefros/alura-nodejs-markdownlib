a592386e0b0bd8aa5a1a5e53b8a7abccb1cad38e
Alef Oliveira <alef.oliveira@gertec.com.br>
2022-08-06 23:07:42 -0300

    Add method to read file on asynchronous way

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const chalk = require('chalk');
 const fs = require('fs');
 
+const ASSETS_PATH = './files/';
+const FILE_PATH =  ASSETS_PATH + 'text1.md';
+
+function treatError(error) {
+    throw new Error(chalk.red(error.code, 'Error on access this file'));
+}
+
+function getFileAsync(filePath) {
+    const encoding = 'utf-8';
+    
+    fs.promises
+    .readFile(filePath, encoding)
+    .then((text) => chalk.green(console.log(text)))
+    .catch((error) => treatError(error));
+    
+}
+
 function getFile(filePath) {
     const encoding = 'utf-8';
-    fs.readFile(filePath, encoding, (_, data) => {
+    fs.readFile(filePath, encoding, (error, data) => {
+        if (error) {
+            treatError(error);
+        }
         console.log(chalk.green(data));
     });
 }
 
-getFile('./files/text1.md');
+getFile(FILE_PATH);
+getFileAsync(FILE_PATH);
